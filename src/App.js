466c2c09e850b45c328edfa0aa12dae70ee01c08
nao-United92 <naoyauver@icloud.com>
2025-05-09 23:10:58 +0900

    前ページのデータ取得処理の実装

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ function App() {
     setIsLoading(false);
   };
 
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    if (prevPage < 1) return;
+    await fetchCharacters(prevPage);
+    setPage(prevPage);
+  };
+
   const handleNext = async () => {
     const nextPage = page + 1;
     await fetchCharacters(nextPage);
@@ -60,7 +67,9 @@ function App() {
             })}
           </div>
           <div className="pager">
-            <button className="prev">Previous</button>
+            <button className="prev" onClick={handlePrev}>
+              Previous
+            </button>
             <span className="page-number">{page}</span>
             <button className="next" onClick={handleNext}>
               Next
