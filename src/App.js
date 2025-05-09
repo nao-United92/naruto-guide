eccdb2388bb0856ad513a4a99b004e1a347fc190
nao-United92 <naoyauver@icloud.com>
2025-05-09 22:49:01 +0900

    取得データの画面表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -11,8 +12,43 @@ function App() {
     const apiUrl = 'http://localhost:3000/character';
 
     const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
+    console.log(result);
   };
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => {
+            return (
+              <div className="card" key={character.id}>
+                <img
+                  src={
+                    character.images[0] != null
+                      ? character.images[0]
+                      : 'dummy.png'
+                  }
+                  alt="character"
+                  className="card-image"
+                />
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">
+                    {character.debut?.appearsIn ?? 'なし'}
+                  </p>
+                  <div className="card-footer">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? 'なし'}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
