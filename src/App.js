4f58be10ed4c71fe3a0270444e9683102f44902b
nao-United92 <naoyauver@icloud.com>
2025-05-09 22:13:28 +0900

    axiosでreactからAPIをコール

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = 'http://localhost:3000/character';
+
+    const result = await axios.get(apiUrl);
+  };
   return <div className="App"></div>;
 }
 
